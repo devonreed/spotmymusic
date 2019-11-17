3acf2ef03a164f077c9a557da4aef1bbcf229c5e
Devon Reed <devonnigelreed@gmail.com>
2019-11-17 17:45:45 -0500

    Meta tag and images

--- resources/views/song.blade.php
+++ resources/views/song.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta itemprop="name" content="What's Your Song?">
     <meta itemprop="description" content="What unique song identifies you on Spotify?">
+    <meta property="og:image" content="http://www.spotmytrack.com/images/sample.jpg">
     
     <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
--- webpack.mix.js
+++ webpack.mix.js
@@ -17,4 +17,5 @@ mix.js('resources/js/app.js', 'public/js')
     .options({
         processCssUrls: false,
         postCss: [ tailwindcss('./tailwind.js') ],
-    });
+    })
+    .copyDirectory('resources/images', 'public/images');
