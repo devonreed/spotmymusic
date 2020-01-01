2616e8123cee0d8c434d5059310faa701e579f26
Devon Reed <devonnigelreed@gmail.com>
2019-12-31 20:21:32 -0500

    Info text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,7 @@
 </head>
 <body class="bg-gray-100 font-sans leading-normal tracking-normal" style="font-family: Libre Franklin">
     @if ($name)
-    <nav id="header" class="bg-white fixed w-full z-10 top-0 shadow">
+    <nav id="header" class="px-5 bg-white fixed w-full z-10 top-0 shadow">
         <div class="w-full container mx-auto flex flex-wrap items-center mt-0 pt-3 pb-3">
                 
             <div class="w-1/2 pl-2 md:pl-0">
@@ -47,10 +47,22 @@
                 <div><label><input class="venue" type="checkbox" value="{{$venue['id']}}" {{$venue['checked']}}>&nbsp;{{$venue['name']}}</label></div>
             @endforeach
         @else
-            <h1 class="text-3xl">Login with Spotify</h1>
+            <h1 class="text-3xl">Spot My Music</h1>
+            <h1 class="text-xl mb-5">Custom Venue-Specific Spotify Playlists</h1>
             <p id="infotext">Click the login button to connect to Spotify and grant access to create a playlist on your Spotify account.</p>
+            <div><a class="modal-open text-sm underline" href="/">How this works</a></div>
             <button id="loginbtn" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-10">Login</button>
         @endif
+    </div>
+
+    <!--Modal-->
+    <div class="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center">
+        <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+        <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+            <div class="modal-content py-4 text-left px-10 py-10">
+                <p>When you grant Spotify permissions to Spot My Music, we will create a custom playlist for you. You can configure the contents of this playlist by selecting venues across your city, and we will populate the playlist each day with songs from artists that are performing at those venues that night.</p>
+            </div>
+        </div>
     </div>
 
     <script>
@@ -66,7 +78,6 @@
             var venues = $('.venue:checked').map(function() {
                 return $(this).val();
             }).get();
-            console.log(venues);
             $.post("/venues", { venue_ids: venues });
         });
     </script>
@@ -78,6 +89,44 @@
         var $logoutBtn = $('#logoutbtn').click(function (e) {
             document.location = "/logout";
         })
+
+        var openmodal = document.querySelectorAll('.modal-open')
+        for (var i = 0; i < openmodal.length; i++) {
+            openmodal[i].addEventListener('click', function (event) {
+                event.preventDefault()
+                toggleModal()
+            });
+        }
+
+        const overlay = document.querySelector('.modal-overlay')
+        overlay.addEventListener('click', toggleModal)
+
+        var closemodal = document.querySelectorAll('.modal-close')
+        for (var i = 0; i < closemodal.length; i++) {
+        closemodal[i].addEventListener('click', toggleModal)
+        }
+
+        document.onkeydown = function(evt) {
+            evt = evt || window.event
+            var isEscape = false
+            if ("key" in evt) {
+                isEscape = (evt.key === "Escape" || evt.key === "Esc")
+            } else {
+                isEscape = (evt.keyCode === 27)
+            }
+            if (isEscape && document.body.classList.contains('modal-active')) {
+                toggleModal()
+            }
+        };
+
+
+        function toggleModal () {
+            const body = document.querySelector('body')
+            const modal = document.querySelector('.modal')
+            modal.classList.toggle('opacity-0')
+            modal.classList.toggle('pointer-events-none')
+            body.classList.toggle('modal-active')
+        }
     </script>
 </body>
 </html>
